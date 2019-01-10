9a184d53136a1864df685f70f6215b7308f6b765
vovka26 <317110@gmail.com>
2019-01-10 10:10:24 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,29 +1,29 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   debugger;
+//   if(isNaN(interval)) {
+//
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
 
-function saveLincoln(interval) {
-  debugger;
-  if(isNaN(interval)) {
-    
-    interval = 10000
+
+
+let button = document.querySelector('#save_lincoln')
+
+function fixLincoln() {
+  let intervalInput = document.querySelector('#interval')
+  let zombie = document.querySelector('#foreground')
+  const removeZombie = function(){zombie.parentElement.removeChild(zombie)}
+  let parsedInterval = parseInt(intervalInput.value)
+  if (isNaN(parsedInterval)) {
+    setTimeout(removeZombie, 5000);
+  }else{
+    setTimeout(removeZombie, parsedInterval);
   }
-  $('#foreground').fadeOut(interval);
+
 }
 
-
-
-// let button = document.querySelector('#save_lincoln')
-//
-// function fixThatBitch() {
-//   let intervalInput = document.querySelector('#interval')
-//   let zombie = document.querySelector('#foreground')
-//   const removeZombie = function(){zombie.parentElement.removeChild(zombie)}
-//   let parsedInterval = parseInt(intervalInput.value)
-//   if (isNaN(parsedInterval)) {
-//     setTimeout(removeZombie, 5000);
-//   }else{
-//     setTimeout(removeZombie, parsedInterval);
-//   }
-//
-// }
-//
-// button.addEventListener('click', fixThatBitch)
+button.addEventListener('click', fixThatBitch)
